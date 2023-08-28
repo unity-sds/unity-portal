5fed8ff81500a88524b9b99de66cbd994c544343
Anil Natha <Anil.Natha@jpl.nasa.gov>
2023-08-28 14:33:07 -0700

    Added environment config to support local development and deployments.

--- src/Config.tsx
+++ src/Config.tsx
@@ -1,21 +1,21 @@
 const Config = {
    
    ['getGlobalConfig']: {
-      oauth_client_id: '7ffsnujgue95dt8bkakfiqhonv',
-      oauth_redirect_uri: 'http://localhost:8080',
-      oauth_logout_endpoint: 'https://unitysds.auth.us-west-2.amazoncognito.com/logout',
-      oauth_provider_url: 'https://unitysds.auth.us-west-2.amazoncognito.com/oauth2',
-      app_admin_group_name: 'Unity_Admin',
-      app_viewer_group_name: 'Unity_Viewer',
-      unity_admin_email: 'anil.natha@jpl.nasa.gov'
+      oauth_client_id: import.meta.env.VITE_AUTH_OAUTH_CLIENT_ID,
+      oauth_redirect_uri: import.meta.env.VITE_AUTH_OAUTH_REDIRECT_URI,
+      oauth_logout_endpoint: import.meta.env.VITE_AUTH_OAUTH_LOGOUT_ENDPOINT,
+      oauth_provider_url: import.meta.env.VITE_AUTH_OAUTH_PROVIDER_URL,
+      app_admin_group_name: import.meta.env.VITE_AUTH_APP_ADMIN_GROUP_NAME,
+      app_viewer_group_name: import.meta.env.VITE_AUTH_APP_APP_VIEWER_GROUP_NAME,
+      unity_admin_email: import.meta.env.VITE_ADMIN_EMAIL
    },
 
    ['sps']: {
-      endpoint: 'http://a0e8898ec12884cc28788c9c5b26d90d-762439384.us-west-2.elb.amazonaws.com:5001/'
+      endpoint: import.meta.env.VITE_SPS_WPST_ENDPOINT
    },
 
    ['ads']: {
-      url: 'http://uads-test-dockstore-deploy-lb-1762603872.us-west-2.elb.amazonaws.com:9998'
+      url: import.meta.env.VITE_ADS_URL
    }
 
 }
