4c55485606eaf6b18c5669b955261a9898fb6a48
Anil Natha <Anil.Natha@jpl.nasa.gov>
2024-04-29 14:56:20 -0700

    Stubbed in new health dashboard view and links to the health dashboard from navbar.

---
+++ src/routes/health-dashboard/index.tsx
@@ -0,0 +1,18 @@
+import { DocumentMeta } from "../../components/DocumentMeta/DocumentMeta"
+
+function HealthDashboard() {
+
+   return (
+      <>
+         <DocumentMeta
+            title="Health Dashboard"
+            description="Health Dashboard"
+         />
+         <div className="main-view">
+            <h1>Health Dashboard</h1>
+         </div>
+      </>
+   )
+}
+
+export default HealthDashboard
