8c8482f5e991e419ad41f6901b23f6322d1b8afa
Anil Natha <Anil.Natha@jpl.nasa.gov>
2024-09-17 16:16:45 -0700

    Commented out instances where we reference Cognito client ID as we don't presently have access to that information from our AWS SSM parameter store.

--- src/Config.tsx
+++ src/Config.tsx
@@ -8,7 +8,7 @@ const Config = {
    },
    
    ['auth']: {
-      oauth_client_id: import.meta.env.VITE_AUTH_OAUTH_CLIENT_ID,
+      //oauth_client_id: import.meta.env.VITE_AUTH_OAUTH_CLIENT_ID,
       oauth_redirect_uri: import.meta.env.VITE_AUTH_OAUTH_REDIRECT_URI,
       oauth_logout_endpoint: import.meta.env.VITE_AUTH_OAUTH_LOGOUT_ENDPOINT,
       oauth_provider_url: import.meta.env.VITE_AUTH_OAUTH_PROVIDER_URL,
--- src/utils/auth.tsx
+++ src/utils/auth.tsx
@@ -1,7 +1,7 @@
 import Config from '../Config';
 
 // OAuth2 configs
-const OAUTH2_CLIENT_ID = Config['auth'].oauth_client_id;
+//const OAUTH2_CLIENT_ID = Config['auth'].oauth_client_id;
 const OAUTH2_REDIRECT_URI = Config['auth'].oauth_redirect_uri;
 const OAUTH2_LOGOUT_ENDPOINT = Config['auth'].oauth_logout_endpoint;
 
@@ -12,7 +12,7 @@ export const logout = async () => {
   console.log("Logging User Out...")
   window.localStorage.clear();
   const logoutUrl = OAUTH2_LOGOUT_ENDPOINT +
-      "?client_id=" + OAUTH2_CLIENT_ID +
+      //"?client_id=" + OAUTH2_CLIENT_ID +
       "&logout_uri=" + OAUTH2_REDIRECT_URI;
   window.location.replace(logoutUrl);
 }
