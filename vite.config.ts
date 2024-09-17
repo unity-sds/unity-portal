13bd0568ae96f715aca32c6d8f94f99787117f5e
Anil Natha <Anil.Natha@jpl.nasa.gov>
2024-09-17 16:16:08 -0700

    Temporarily disabled minifying code to resolve issue with icons not rendering correctly in instance of AGGrid.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   envDir: ".env",
+  build: {
+    minify: true
+  },
   plugins: [
     react()
   ],
