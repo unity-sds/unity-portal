fde9610c9e6b808a96d333f89d8fe84c1b84ab66
Anil Natha <Anil.Natha@jpl.nasa.gov>
2024-12-17 11:13:59 -0800

    Updated the "dashboard" proxy name to be "ui" instead to correspond to changes made to our terraform configuration.

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter basename={`${Config['general']['project']}/${Config['general']['venue']}/dashboard`}>
+      <BrowserRouter basename={`${Config['general']['project']}/${Config['general']['venue']}/ui`}>
         <Routes>
           <Route path="*" Component={AuthorizationWrapper}/>
         </Routes>
