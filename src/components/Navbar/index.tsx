af10b7c97a460b1ac488b9fa9e3fb0c473f5ae7d
Anil Natha <Anil.Natha@jpl.nasa.gov>
2025-03-17 13:56:49 -0700

    Fixed code indentation and resolved SonarQube warnings about single child elements contained in fragments being redundant.

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -28,9 +28,7 @@ import Config from "../../Config";
 import { formatRoute } from "../../utils/strings";
 
 const MenuErrorMessage = ({message}:{message:string}) => {
-  return <>
-    <div className="st-react-menu-message"><IconWarning />{message}</div>
-  </>
+  return <div className="st-react-menu-message"><IconWarning />{message}</div>
 }
 
 export default function Navbar() {
@@ -66,177 +64,175 @@ export default function Navbar() {
 
   }, [dispatch, healthState]);
 
-   return (
-      <>
-         <StellarNavbar mobileBreakpoint={800}>
-            <NavbarBreakpoint min={1100}>
-               <NavbarBrand
-                  link={basePath}
-                  logo={<img src={basePath + MdpsLogo} alt={appTitle + " logo"} style={{ height: '24px', width: '24px' }}/>}
-                  title={appTitle}
-                  version={uiVersion}
-               />
-               <NavbarContent
-                  align="right"
-                  full
-                  responsiveBreakpointMin={1100}
-               >
-                  <div
-                     style={{
-                        alignItems: 'center',
-                        display: 'flex',
-                        gap: 'var(--st-grid-unit2x)'
-                     }}
-                  >
-                     <Menu trigger={
-                        <Button size="large" style={{ gap: '4px', padding: '0 var(--st-grid-unit)' }} variant="tertiary">
-                           <IconThreeDot />
-                           <IconChevronDown />
-                        </Button>
-                     }>
-                        <NavLink to="/"><MenuItem>Home</MenuItem></NavLink>
-                        <NavLink to="/health-dashboard"><MenuItem>Health Dashboard</MenuItem></NavLink>
-                        {
-                          healthState.items.map( (service, index) => {
-                            return <NavLink to={"/applications/" + formatRoute(service.componentName)} key={index}>
-                              <MenuItem>{service.componentName}</MenuItem>
-                            </NavLink>
-                          })
-                        }
-                        <NavLink to="https://unity-sds.gitbook.io/docs" target="_blank">
-                          <MenuItem>Documentation (Gitbook)</MenuItem>
-                        </NavLink>
-                        {
-                          healthApiError && <MenuErrorMessage message={healthApiErrorMessage} />
-                        }
-                     </Menu>
-                     <Menu trigger={
-                        <Button size="large" style={{ gap: '4px', padding: '0 var(--st-grid-unit)' }} variant="tertiary">
-                           <Avatar text={userInitials} /><IconChevronDown />
-                        </Button>
-                     }>
-                        <MenuLabel>
-                           Welcome {loggedInUsername}
-                        </MenuLabel>
-                        <MenuItem>
-                           Account Settings
-                        </MenuItem>
-                        <MenuItem onClick={logout}>
-                           Logout
-                           <MenuRightSlot>
-                              <IconArrowRight />
-                           </MenuRightSlot>
-                        </MenuItem>
-                     </Menu>
-                  </div>
-               </NavbarContent>
-            </NavbarBreakpoint>
-            <NavbarBreakpoint
-               max={1100}
-               min={800}
-            >
-               <NavbarBrand
-                  link={basePath}
-                  logo={<img src={basePath + MdpsLogo} alt={appTitle + " logo"} style={{ height: '24px', width: '24px' }}/>}
-                  title={appTitle}
-                  version={uiVersion}
-               />
-               <NavbarContent
-                  align="right"
-                  full
-               >
-                  <div
-                     style={{
-                        alignItems: 'center',
-                        display: 'flex',
-                        gap: 'var(--st-grid-unit2x)'
-                     }}
-                  >
-                     <Menu trigger={
-                        <Button size="large" style={{ gap: '4px', padding: '0 var(--st-grid-unit)' }} variant="tertiary">
-                           <IconThreeDot />
-                           <IconChevronDown />
-                        </Button>
-                     }>
-                        <NavLink to="/"><MenuItem>Home</MenuItem></NavLink>
-                        <NavLink to="/health-dashboard"><MenuItem>Health Dashboard</MenuItem></NavLink>
-                        {
-                          healthState.items.map( (service, index) => {
-                            return <NavLink to={"/applications/" + formatRoute(service.componentName)} key={index}>
-                              <MenuItem>{service.componentName}</MenuItem>
-                            </NavLink>
-                          })
-                        }
-                        <NavLink to="https://unity-sds.gitbook.io/docs" target="_blank">
-                          <MenuItem>Documentation (Gitbook)</MenuItem>
-                        </NavLink>
-                        {
-                          healthApiError && <MenuErrorMessage message={healthApiErrorMessage} />
-                        }
-                     </Menu>
-                     <Menu trigger={
-                        <Button size="large" style={{ gap: '4px', padding: '0 var(--st-grid-unit)' }} variant="tertiary">
-                           <Avatar text={userInitials} />
-                           <IconChevronDown />
-                        </Button>}>
-                        <MenuLabel>
-                           Welcome {loggedInUsername}
-                        </MenuLabel>
-                        <MenuItem>
-                           Account Settings
-                        </MenuItem>
-                        <MenuItem onClick={logout}>
-                           Logout
-                           <MenuRightSlot>
-                              <IconArrowRight />
-                           </MenuRightSlot>
-                        </MenuItem>
-                     </Menu>
-                  </div>
-               </NavbarContent>
-            </NavbarBreakpoint>
-            <NavbarBreakpoint max={800}>
-               <NavbarContent
-                  align="center"
-                  full
-               >
-                  <NavbarBrand title={appTitle} />
-               </NavbarContent>
-               <NavbarContent align="right">
-                  <Menu trigger={
-                     <Button size="large" style={{ gap: '4px', padding: '0 var(--st-grid-unit)' }} variant="tertiary">
-                        <Avatar text={userInitials} />
-                        <IconChevronDown />
-                     </Button>}>
-                     <MenuLabel>
-                        Welcome {loggedInUsername}
-                     </MenuLabel>
-                     <MenuItem>
-                        Account Settings
-                     </MenuItem>
-                     <MenuItem onClick={logout}>
-                        Logout
-                        <MenuRightSlot>
-                           <IconArrowRight />
-                        </MenuRightSlot>
-                     </MenuItem>
-                  </Menu>
-               </NavbarContent>
-            </NavbarBreakpoint>
-            <NavbarMobileMenu>
-               <NavLink to="/" className="st-react-navbar-link"><IconHome />{' '}Home</NavLink>
-               <NavLink to="/health-dashboard" className="st-react-navbar-link">{' '}Health Dashboard</NavLink>
-               {
-                 healthState.items.map( (service, index) => {
-                   return <NavLink key={index} className="st-react-navbar-link" to={"/applications/" + formatRoute(service.componentName)}>{service.componentName}</NavLink>
-                 })
-               }
-               <NavLink to="https://unity-sds.gitbook.io/docs" target="_blank" className="st-react-navbar-link">{' '}Documentation (Gitbook)</NavLink>
-               {
+  return (
+    <StellarNavbar mobileBreakpoint={800}>
+      <NavbarBreakpoint min={1100}>
+        <NavbarBrand
+          link={basePath}
+          logo={<img src={basePath + MdpsLogo} alt={appTitle + " logo"} style={{ height: '24px', width: '24px' }}/>}
+          title={appTitle}
+          version={uiVersion}
+        />
+        <NavbarContent
+          align="right"
+          full
+          responsiveBreakpointMin={1100}
+        >
+          <div
+            style={{
+              alignItems: 'center',
+              display: 'flex',
+              gap: 'var(--st-grid-unit2x)'
+            }}
+          >
+            <Menu trigger={
+              <Button size="large" style={{ gap: '4px', padding: '0 var(--st-grid-unit)' }} variant="tertiary">
+                  <IconThreeDot />
+                  <IconChevronDown />
+              </Button>
+            }>
+              <NavLink to="/"><MenuItem>Home</MenuItem></NavLink>
+              <NavLink to="/health-dashboard"><MenuItem>Health Dashboard</MenuItem></NavLink>
+              {
+                healthState.items.map( (service, index) => {
+                  return <NavLink to={"/applications/" + formatRoute(service.componentName)} key={index}>
+                    <MenuItem>{service.componentName}</MenuItem>
+                  </NavLink>
+                })
+              }
+              <NavLink to="https://unity-sds.gitbook.io/docs" target="_blank">
+                <MenuItem>Documentation (Gitbook)</MenuItem>
+              </NavLink>
+              {
                 healthApiError && <MenuErrorMessage message={healthApiErrorMessage} />
-               }
-            </NavbarMobileMenu>
-         </StellarNavbar>
-      </>
-   )
+              }
+            </Menu>
+            <Menu trigger={
+              <Button size="large" style={{ gap: '4px', padding: '0 var(--st-grid-unit)' }} variant="tertiary">
+                  <Avatar text={userInitials} /><IconChevronDown />
+              </Button>
+            }>
+              <MenuLabel>
+                  Welcome {loggedInUsername}
+              </MenuLabel>
+              <MenuItem>
+                  Account Settings
+              </MenuItem>
+              <MenuItem onClick={logout}>
+                  Logout
+                  <MenuRightSlot>
+                    <IconArrowRight />
+                  </MenuRightSlot>
+              </MenuItem>
+            </Menu>
+          </div>
+        </NavbarContent>
+      </NavbarBreakpoint>
+      <NavbarBreakpoint
+          max={1100}
+          min={800}
+      >
+        <NavbarBrand
+          link={basePath}
+          logo={<img src={basePath + MdpsLogo} alt={appTitle + " logo"} style={{ height: '24px', width: '24px' }}/>}
+          title={appTitle}
+          version={uiVersion}
+        />
+        <NavbarContent
+          align="right"
+          full
+        >
+          <div
+              style={{
+                alignItems: 'center',
+                display: 'flex',
+                gap: 'var(--st-grid-unit2x)'
+              }}
+          >
+            <Menu trigger={
+              <Button size="large" style={{ gap: '4px', padding: '0 var(--st-grid-unit)' }} variant="tertiary">
+                  <IconThreeDot />
+                  <IconChevronDown />
+              </Button>
+            }>
+              <NavLink to="/"><MenuItem>Home</MenuItem></NavLink>
+              <NavLink to="/health-dashboard"><MenuItem>Health Dashboard</MenuItem></NavLink>
+              {
+                healthState.items.map( (service, index) => {
+                  return <NavLink to={"/applications/" + formatRoute(service.componentName)} key={index}>
+                    <MenuItem>{service.componentName}</MenuItem>
+                  </NavLink>
+                })
+              }
+              <NavLink to="https://unity-sds.gitbook.io/docs" target="_blank">
+                <MenuItem>Documentation (Gitbook)</MenuItem>
+              </NavLink>
+              {
+                healthApiError && <MenuErrorMessage message={healthApiErrorMessage} />
+              }
+            </Menu>
+            <Menu trigger={
+              <Button size="large" style={{ gap: '4px', padding: '0 var(--st-grid-unit)' }} variant="tertiary">
+                  <Avatar text={userInitials} />
+                  <IconChevronDown />
+              </Button>}>
+              <MenuLabel>
+                  Welcome {loggedInUsername}
+              </MenuLabel>
+              <MenuItem>
+                  Account Settings
+              </MenuItem>
+              <MenuItem onClick={logout}>
+                  Logout
+                  <MenuRightSlot>
+                    <IconArrowRight />
+                  </MenuRightSlot>
+              </MenuItem>
+            </Menu>
+          </div>
+        </NavbarContent>
+      </NavbarBreakpoint>
+      <NavbarBreakpoint max={800}>
+        <NavbarContent
+          align="center"
+          full
+        >
+          <NavbarBrand title={appTitle} />
+        </NavbarContent>
+        <NavbarContent align="right">
+          <Menu trigger={
+            <Button size="large" style={{ gap: '4px', padding: '0 var(--st-grid-unit)' }} variant="tertiary">
+              <Avatar text={userInitials} />
+              <IconChevronDown />
+            </Button>}>
+            <MenuLabel>
+              Welcome {loggedInUsername}
+            </MenuLabel>
+            <MenuItem>
+              Account Settings
+            </MenuItem>
+            <MenuItem onClick={logout}>
+              Logout
+              <MenuRightSlot>
+                  <IconArrowRight />
+              </MenuRightSlot>
+            </MenuItem>
+          </Menu>
+        </NavbarContent>
+      </NavbarBreakpoint>
+      <NavbarMobileMenu>
+        <NavLink to="/" className="st-react-navbar-link"><IconHome />{' '}Home</NavLink>
+        <NavLink to="/health-dashboard" className="st-react-navbar-link">{' '}Health Dashboard</NavLink>
+        {
+          healthState.items.map( (service, index) => {
+            return <NavLink key={index} className="st-react-navbar-link" to={"/applications/" + formatRoute(service.componentName)}>{service.componentName}</NavLink>
+          })
+        }
+        <NavLink to="https://unity-sds.gitbook.io/docs" target="_blank" className="st-react-navbar-link">{' '}Documentation (Gitbook)</NavLink>
+        {
+        healthApiError && <MenuErrorMessage message={healthApiErrorMessage} />
+        }
+      </NavbarMobileMenu>
+    </StellarNavbar>
+  )
 }
